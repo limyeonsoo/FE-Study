b6ae97ed54d1936edbc1ba7f0fa472f2ffd7f8d7
limyeonsoo <yslim6168@naver.com>
2021-02-05 00:51:34 +0900

    Store, Reducer, Action,

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const countModifier = (count = 0, action) => {
+  if  (action.type === "ADD"){
+    return count+1;
+  }
+  else if (action.type === "MINUS"){
+    return count-1;
+  }
+  return count;
+}
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+const countStore = createStore(countModifier);
+countStore.subscribe(onChange);
+
+add.addEventListener('click', ()=>{
+  countStore.dispatch({type:"ADD"})
+})
+minus.addEventListener('click', ()=>{
+  countStore.dispatch({type:"MINUS"});
+})
