a9b79a7b491c71bcc4873f75c5fb0823cb6b6732
limyeonsoo <yslim6168@naver.com>
2021-04-03 12:35:32 +0900

    정적파일 제공

--- SSR Practice/ssr-recipe/src/index.server.js
+++ SSR Practice/ssr-recipe/src/index.server.js
@@ -4,11 +4,51 @@ import ReactDOMServer from 'react-dom/server';
 import express from 'express';
 import { StaticRouter } from 'react-router-dom';
 import App from './App';
+import path from 'path';
+import fs from 'fs';
+
+// build/asset-manifest.json 파일 경로 조회.
+const manifest = JSON.parse(
+    fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf8')
+);
+
+//chunk.js로 끝나는 키를 찾아 / 스크립트 태그로 변환 후 / 합쳐주어야함.
+const chunks = Object.keys(manifest.files)
+    .filter(key => /chunk\.js$/.exec(key)) 
+    .map(key => `<script src="${manifest.files[key]}"></script>`)
+    .join('');
+
+function createPage(root) {
+    return `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="utf-8"/>
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <meta
+                name="viewport"
+                content="width=device-width,initial-scale=1,shrink-to-fit=no"
+            />
+            <meta name="theme-color" content="#000000" />
+            <title>React App</title>
+            <link href="${manifest.files['main.css']}" rel="stylesheet" />
+        </head>
+        <body>
+            <noscript>You need to enable JavaScript to run this app.</noscript>
+            <div id="root">
+                ${root}
+            </div>
+            <script src="${manifest.files['runtime-main.js']}"></script>
+            ${chunks}
+            <script src="${manifest.files['main.js']}"></script>
+        </body>
+        </html>
+        `;
+}
 
 const app = express();
 
 // Server Side Rendering  Handler
-const serverRender = (req, res, next) => {
+const serverRender = async(req, res, next) => {
     // 404가 떠야할 때 404 대 신 서버 사이드 렌더링 진행.
     const context = {};
     const jsx = (
@@ -17,9 +57,14 @@ const serverRender = (req, res, next) => {
         </StaticRouter>
     );
     const root = ReactDOMServer.renderToString(jsx);
-    res.send(root);
+    res.send(createPage(root));
 }
 
+const serve = express.static(path.resolve('./build'), {
+    index:false  //  "/" 경로에서  'index.html' 을 숨김.
+})
+
+app.use(serve);  // 순서 중요.
 app.use(serverRender);
 
 app.listen(5000, () => {
