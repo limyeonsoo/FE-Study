9819c8f013e521be519a394665bc4f0cf1861c9b
limyeonsoo <yslim6168@naver.com>
2021-05-28 00:39:36 +0900

    버블링 추상화  -> 메소드

--- JavaScript/Component/src/components/Items.js
+++ JavaScript/Component/src/components/Items.js
@@ -1,38 +1,34 @@
 import Component from "../core/Component.js";
 
 export default class Items extends Component {
-  setup () {
-    this.$state = { items: ['item1', 'item2'] };
-  }
-  template () {
-    const { items } = this.$state;
-    return `
-      <ul>
-        ${items.map((item, key) => `
-          <li>
-            ${item}
-            <button class="deleteBtn" data-index="${key}">삭제</button>
-          </li>
-        `).join('')}
-      </ul>
-      <button class="addBtn">추가</button>
-    `
-  }
+    setup () {
+        this.$state = { items: ['item1', 'item2'] };
+    }
+    
+    template () {
+        const { items } = this.$state;
+        return `
+        <ul>
+            ${items.map((item, key) => `
+            <li>
+                ${item}
+                <button class="deleteBtn" data-index="${key}">삭제</button>
+            </li>
+            `).join('')}
+        </ul>
+        <button class="addBtn">추가</button>
+        `
+    }   
 
-  setEvent () {
-    // 모든 이벤트를 this.$target에 등록하여 사용하면 된다.
-    this.$target.addEventListener('click', ({ target }) => {
-      const items = [ ...this.$state.items ];
-
-      if (target.classList.contains('addBtn')) {
-        this.setState({ items: [ ...items, `item${items.length + 1}` ] });
-      }
-
-      if (target.classList.contains('deleteBtn')) {
-        items.splice(target.dataset.index, 1);
-        this.setState({ items });
-      }
-
-    });
-  }
+    setEvent () {
+        this.addEvent('click', '.addBtn', ({ target }) => {
+            const { items } = this.$state;
+            this.setState({ items: [ ...items, `item${items.length + 1}` ] });
+        });
+        this.addEvent('click', '.deleteBtn', ({ target }) => {
+            const items = [ ...this.$state.items ];
+            items.splice(target.dataset.index, 1);
+            this.setState({ items });
+        });
+    }
 }
