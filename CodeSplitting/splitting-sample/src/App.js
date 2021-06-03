7b810d587db3ee4fc7c569e2b10a5aef331d10d5
limyeonsoo <yslim6168@naver.com>
2021-06-03 12:22:57 +0900

    class Component를 이용한 Split 상태 관리

--- CodeSplitting/splitting-sample/src/App.js
+++ CodeSplitting/splitting-sample/src/App.js
@@ -1,19 +1,28 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const onClick = () => {
-    import('./notify').then(result => result.default());
+class App extends Component{
+  state = {
+    SplitMe: null
+  };
+  handleClick = async() => {
+    const loadedModule = await import('./SplitMe');
+    this.setState({
+      SplitMe: loadedModule.default
+    })
+  };
+  render(){
+    const { SplitMe } = this.state;
+    return(
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handleClick}>Hello React!</p>
+            {SplitMe && <SplitMe />}
+        </header>
+      </div>
+    );
   }
-  // 클릭하는 시점에 새로운 자바스크립트 파일을 불러온다.
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}> Edit <code>src/App.js</code> and save to reload.</p>
-      </header>
-    </div>
-  );
 }
-
 export default App;
