f7e411cfa7aa875a39aab25e754e83a6370f5736
limyeonsoo <yslim6168@naver.com>
2021-06-03 12:05:15 +0900

    클릭하는 시점에 새로운 자바스크립트 파일을 불러온다.

--- CodeSplitting/splitting-sample/src/App.js
+++ CodeSplitting/splitting-sample/src/App.js
@@ -5,6 +5,7 @@ function App() {
   const onClick = () => {
     import('./notify').then(result => result.default());
   }
+  // 클릭하는 시점에 새로운 자바스크립트 파일을 불러온다.
   return (
     <div className="App">
       <header className="App-header">
